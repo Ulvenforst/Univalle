43f11e7a1dcaf74698b7a0da6143b8835d8d4bcf
Juan Camilo Narváez <66099723+Ulvenforst@users.noreply.github.com>
2022-05-03 09:17:43 -0500

    Add files via upload

---
+++ Camion.h
@@ -0,0 +1,62 @@
+/*
+  Archivo: Camion.cpp
+  Autor: Juan Camilo Narváez Tascón <juan.narvaez.tascon@correounivalle.edu.co>
+  Fecha creación: 2022-05-02
+  Fecha última modificación: 2022-05-02
+  Licencia: GNU-GPL
+*/
+
+/**
+  CLASE: Camion
+  INTENCIÓN: Obtener la placa de identificación del camión, al igual que la capacidad máxima de peso que puede transportar. Con estos datos se guardarán objetos en el camión, mientras no excedan la capacidad máxima de peso, y se transportarán, indicando al final cuánto fue lo máximo que pudo transportar. Por último el camión se vaciará.
+  RELACIONES: ninguna
+*/
+
+#ifndef CAMION_H
+#define CAMION_H
+
+#include<string>
+using namespace std;
+
+class Camion
+{
+  private:
+  string placa;
+  int capacidadMaxima;
+  int pesoAcomulado;
+  bool mudanzaRealizada;
+
+  public:
+  /**
+  Constructor. Da valores iniciales a los atributos internos
+  */
+  Camion(string laPlaca, int laCapacidadMaxima);
+  /**
+  Destructor. No hace nada
+  */
+  virtual ~Camion();
+  /**
+  Retorna la identificación de la placa.
+  */
+  virtual string cualEsLaPlaca();
+  /**
+  Carga el objeto respectivo, que brinde el vector, al camión (Si este cuenta con la capacidad).
+  */
+  bool cargarUnObjeto(int pesoDelObjeto); // true = se pudo cargar; false = no se pudo cargar
+  /**
+  Validará que la mudanza se haya hecho.
+  */
+  virtual void hacerMudanza();
+  /**
+  Si la mudanza fue hecha, retornará el peso que transportó.
+  */
+  virtual int pesoTransportado(); // Retorna la carga que transportó
+  /**
+  Vacía los camiones dejando el peso acomulado en 0.
+  */
+  virtual void descargar();
+};
+
+#else
+class Camion; // Declaración adelantada
+#endif
---
+++ main.cpp
@@ -0,0 +1,52 @@
+/*
+  Archivo: main.cpp
+  Autor: Juan Camilo Narváez Tascón <juan.narvaez.tascon@correounivalle.edu.co>
+  Fecha creación: 2022-05-02
+  Fecha última modificación: 2022-05-02
+  Licencia: GNU-GPL
+*/
+
+// Ejemplo con una clase y dos objetos.
+/** 
+Hacer una mudanza. Tenemos dos camiones y un conjunto de objetos a transportar, para hacer una mudanza. 
+  Cada objeto lo intentamos subir al primer camión, pero si no se puede lo llevamos al segundo camión; y si tampoco se puede, lo dejamos en tierra (no se puede transportar). Luego a cada camión le pedimos que haga la mudanza, que nos diga cuanto peso transportó y que descargue los objetos. 
+  Estoy modelando un camión con una placa, que puede transportar una cierta carga en kilos, y que tiene una capacidad máxima en kilos. Se le puede pedir que me diga su placa, que cargue un objeto, que transporte toda la carga y que me diga cuánto transportó y que descargue todo.
+*/
+
+#include "Camion.h"
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  Camion unCamion("ABC-001", 10000);
+  Camion otroCamion("DEF-002", 5000);
+
+  vector<int> objetos = {1000, 3000, 5000, 2000, 500, 1800, 4000, 30};
+
+  for (int cualObjeto = 0; cualObjeto < objetos.size(); cualObjeto++)
+  {
+    int objetoATransportar = objetos[cualObjeto];
+
+    if (not unCamion.cargarUnObjeto(objetoATransportar))
+    {
+      if (not otroCamion.cargarUnObjeto(objetoATransportar))
+      {
+        cout << "El objeto número " << cualObjeto << " que pesa " << objetos[cualObjeto] << " no se puede cargar en ningún camión." << endl;
+      }
+    }
+  }
+
+  unCamion.hacerMudanza();
+  otroCamion.hacerMudanza();
+
+  cout << "El camión " << unCamion.cualEsLaPlaca() << " transportó " << unCamion.pesoTransportado() << " kg." << endl;
+  cout << "El camión " << otroCamion.cualEsLaPlaca() << " transportó " << otroCamion.pesoTransportado() << " kg." << endl;
+
+  unCamion.descargar();
+  otroCamion.descargar();
+
+  return 0;
+}
