f75e3617e921a10556a4d9277bbe6721cf792483
Juan Camilo Narváez <66099723+Ulvenforst@users.noreply.github.com>
2022-08-08 10:50:58 -0500

    Update PresentadorAleatorio.cpp

--- Programación OO/Lab4/PresentadorAleatorio.cpp
+++ Programación OO/Lab4/PresentadorAleatorio.cpp
@@ -1,7 +1,6 @@
 /*
   Archivo: PresentadorAleatorio.cpp
-  Autores: Juan Camilo Narváez Tascón, Carol Sofía Macarena Rubiano Valderrama.
-  Email: <juan.narvaez.tascon@correounivalle.edu.co>, <carol.rubiano@correounivalle.edu.co>
+  Autor: Juan Camilo Narváez Tascón <juan.narvaez.tascon@correounivalle.edu.co>
   Fecha creación: 2020-10-24
   Fecha última modificación: 2022-30-07
   Versión: 0.2
